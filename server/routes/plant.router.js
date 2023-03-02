b0faa9044275b6a2da198e58f91852e0882ba9a1
Peter Lee <PeterLee2094@gmail.com>
2023-03-01 20:13:31 -0600

    added pages files

--- server/routes/plant.router.js
+++ server/routes/plant.router.js
@@ -10,7 +10,7 @@ const {
 router.get("/", rejectUnauthenticated, (req, res) => {
   // GET route code here
   const user = req.user;
-  const queryText = `SELECT * FROM "plant" WHERE "user_id" = $1`;
+  const queryText = `SELECT * FROM "plant" WHERE "user_id" = $1;`;
 
   if (req.isAuthenticated()) {
     pool
@@ -27,6 +27,69 @@ router.get("/", rejectUnauthenticated, (req, res) => {
   }
 });
 
+// Filter by Garden (GET)
+router.get("/filter/garden/:id", rejectUnauthenticated, (req, res) => {
+  if (req.isAuthenticated()) {
+    const user = req.user;
+    const gardenId = req.params.id;
+    const queryText = `SELECT * FROM "plant" WHERE "garden_id" = $1;`;
+
+    pool
+      .query(queryText, [gardenId])
+      .then((results) => {
+        res.send(results.rows);
+      })
+      .catch((err) => {
+        console.log("Error with GETing filtered garden plants: ", err);
+        res.sendStatus(500);
+      });
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+// GET selected plant
+router.get("/selected/:id", rejectUnauthenticated, (req, res) => {
+  if (req.isAuthenticated()) {
+    const plantId = req.params.id;
+    const queryText = `SELECT * FROM "plant" WHERE "id" = $1;`;
+
+    pool
+      .query(queryText, [plantId])
+      .then((results) => {
+        res.send(results.rows);
+      })
+      .catch((err) => {
+        console.log("Error with getting selected plant: ", err);
+      });
+  } else {
+    console.log("Unauthenticated");
+    res.sendStatus(403);
+  }
+});
+
+// GET plant info
+router.get("/info/:id", rejectUnauthenticated, (req, res) => {
+  if (req.isAuthenticated()) {
+    const plantId = req.params.id;
+    const queryText = `SELECT "scientific_name", "sunlight_level", "water_level", "sci_origin", "sci_maintenance", "sci_cycle", "sci_type", "sci_soil", "sci_growth_rate", "scientific_color" FROM "plant_info" 
+        JOIN "plant" ON "plant"."plant_info_id" ="plant_info"."id"
+        WHERE "plant"."id" = $1;`;
+
+    pool
+      .query(queryText, [plantId])
+      .then((results) => {
+        res.send(results.rows);
+      })
+      .catch((err) => {
+        console.log("Error with GETing plant info: ", err);
+        res.sendStatus(500);
+      });
+  } else {
+    console.log("Unauthenticated");
+    res.sendStatus(403);
+  }
+});
 /**
  * POST route template
  */
