323e0c0ea63bd59c386d79e784598b11b166ff80
Peter Lee <PeterLee2094@gmail.com>
2023-03-02 03:13:37 -0600

    added responsive styling to login page"

--- src/components/Login/RegisterForm/RegisterForm.jsx
+++ src/components/Login/RegisterForm/RegisterForm.jsx
@@ -8,6 +8,10 @@ import { FiUnlock } from "react-icons/fi";
 function RegisterForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  const [focused, setFocus] = useState(false);
+  const [passFocus, setPassFocus] = useState(false)
+
   const errors = useSelector((store) => store.errors);
   const dispatch = useDispatch();
 
@@ -23,6 +27,20 @@ function RegisterForm() {
     });
   }; // end registerUser
 
+  const toggleFocusOn = () => {
+    setFocus(true)
+  }
+  const toggleFocusOff = () => {
+    setFocus(false)
+  }
+
+  const togglePassFocusOn = () => {
+    setPassFocus(true)
+  }
+  const togglePassFocusOff = () => {
+    setPassFocus(false)
+  }
+
   return (
     <form className="formPanel" onSubmit={registerUser}>
        <p className={styles.title}>Register User</p>
@@ -31,29 +49,39 @@ function RegisterForm() {
           {errors.registrationMessage}
         </h3>
       )}
-      <div className={styles.input}>
+      <div className={styles.input} style={focused ? { backgroundColor: "rgb(245,245,250)",borderBottom: "solid 1px rgb(100,100,200)"}: {}}>
         <label htmlFor="username">
-          <BiUser size={25} />
+          <BiUser size={25} className={styles.icon} style={focused ? { color: "rgb(0,0,200)"} : {}}/>
         </label>
         <input
           type="text"
           name="username"
           required
           value={username}
+
+          style={focused ? { backgroundColor: "rgb(245,245,250)"} : {}}
+
           onChange={(event) => setUsername(event.target.value)}
+          onFocus={toggleFocusOn}
+          onBlur={toggleFocusOff}
+
           placeholder={"Username"}
         />
       </div>
-      <div className={styles.input}>
+      <div className={styles.input} style={passFocus ? {backgroundColor: "rgb(245,245,250)",borderBottom: "solid 1px rgb(100,100,200)"}: {}}>
         <label htmlFor="password">
-          <FiUnlock size={22}/>
+          <FiUnlock size={22} className={styles.icon} style={passFocus ? { color: "rgb(0,0,200)"} : {}}/>
         </label>
         <input
           type="password"
           name="password"
           required
           value={password}
+
+          style={passFocus ? { backgroundColor: "rgb(245,245,250)"} : {}}
           onChange={(event) => setPassword(event.target.value)}
+          onFocus={togglePassFocusOn}
+          onBlur={togglePassFocusOff}
 
           placeholder={"Password"}
         />
