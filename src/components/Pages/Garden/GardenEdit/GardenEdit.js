c2839f67c63a3e6fcdfadca63728b6006acf56d6
Peter Lee <PeterLee2094@gmail.com>
2023-03-09 16:40:15 -0600

    added garden edit menu

---
+++ src/components/Pages/Garden/GardenEdit/GardenEdit.js
@@ -0,0 +1,8 @@
+import styles from './GardenEdit.module.css'
+export default function GardenEdit(){
+    return(
+        <div className={styles.container}>
+
+        </div>
+    )
+}
--- src/components/Pages/Garden/GardenItem/GardenItem.js
+++ src/components/Pages/Garden/GardenItem/GardenItem.js
@@ -1,20 +1,36 @@
 import { useHistory } from "react-router-dom";
+import { useState } from "react";
 
 import styles from "./GardenItem.module.css";
 import { BsArrowRight } from "react-icons/bs";
+import { RxDotsVertical } from "react-icons/rx";
 
-export default function GardenItem({ id,name, type, num, create }) {
+export default function GardenItem({ id, name, type, num, create }) {
   const history = useHistory();
+  const [overMenu, setOverMenu] = useState(false);
   const gotoGarden = () => {
-    history.push(`/garden/filter/${id}`)
-  }
+    if (!overMenu) {
+      history.push(`/garden/filter/${id}`);
+    }
+  };
   return (
-    <div
-      className={`${styles.container} clickable`}
-      onClick={gotoGarden}
-    >
+    <div className={`${styles.container} clickable`} onClick={gotoGarden}>
       <div className={styles.title}>
         <p>{name}</p>
+        <RxDotsVertical
+          size={20}
+          className={styles.dotMenu}
+          onMouseOver={() => {
+            setOverMenu(true);
+            console.log(overMenu)
+          }}
+          onMouseOut={() => {
+            setOverMenu(false);
+            console.log(overMenu)
+          }}
+
+          onClick={()=>{console.log("hello world")}}
+        />
       </div>
 
       <div className={styles.description}>
