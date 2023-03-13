52c8b172c65586faf3e355406776f7161a8e69b3
Peter Lee <PeterLee2094@gmail.com>
2023-03-13 18:43:21 -0500

    added feature plants

--- src/components/Pages/Home/FeaturePlants/FeaturePlants.js
+++ src/components/Pages/Home/FeaturePlants/FeaturePlants.js
@@ -1,12 +1,32 @@
 import PlantsItem from '../../Plants/PlantItem/PlantItem'
 import styles from './FeaturePlants.module.css'
+import { useHistory } from 'react-router-dom'
+import {AiOutlinePlus} from 'react-icons/ai'
+import { useSelector } from 'react-redux'
 export default function FeaturePlants(){
+    const history = useHistory()
+    const plants = useSelector((store) => store.plant.plantReducer);
+
     return(
         <div className={styles.main}>
-            <PlantsItem/>
-            <PlantsItem/>
-            <PlantsItem/>
-            <PlantsItem/>
+            {plants.map((plant)=>{return(
+                <PlantsItem 
+                key={plant.plant_table_id}
+                id={plant.plant_table_id}
+                name={plant.plant_name}
+                species={plant.scientific_name}/>
+            )})} 
+            <div className={styles.create_container}>
+            <div className={styles.create_image}>
+                <button><AiOutlinePlus size={25} style={{color:"rgb(100,100,100)"}} onClick={()=>{history.push("/plant/create")}}/></button>
+            </div>
+
+
+            {/* <div className={styles.description}>
+                <p className={styles.name}>{}</p>
+                <p className={styles.spes}>{}</p>
+            </div> */}
+        </div>
         </div>
     )
 }
--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -4,23 +4,29 @@ import GardenItem from "../Garden/GardenItem/GardenItem";
 import PlantsItem from "../Plants/PlantItem/PlantItem";
 import FeaturePlants from "./FeaturePlants/FeaturePlants";
 import LogOutButton from "../../Layout/LogOutButton/LogOutButton";
-export default function Home(){
-    const dispatch = useDispatch();
-    
-    const garden = useSelector(store=>store.garden.gardenReducer);
-    const plant = useSelector(store=>store.plant);
 
-    useEffect(()=>{
-        dispatch({type:"FETCH_GARDEN"});
-        dispatch({type:"FETCH_PLANT"})
-    },[])
+import styles from './Home.module.css'
+export default function Home() {
+  const dispatch = useDispatch();
 
-    return(
-        <div className="page_container">
-            <LogOutButton/>
-            <GardenItem/>
-            <FeaturePlants/>
-        </div>
-        
-    )
+  const garden = useSelector((store) => store.garden.gardenReducer);
+  const plant = useSelector((store) => store.plant);
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_GARDEN" });
+    dispatch({ type: "FETCH_PLANT" });
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      
+      <LogOutButton />
+      <GardenItem />
+
+      <div className={styles.feature_plants_container}>
+        <p className={styles.modal_title}>view plants: </p>
+        <FeaturePlants />
+      </div>
+    </div>
+  );
 }
