208fd852ebc3892a43dc16e44265d7aa230125ae
Peter Lee <PeterLee2094@gmail.com>
2023-03-13 20:21:05 -0500

    add explore files

---
+++ src/components/Pages/Home/ExploreGarden/ExploreGarden.js
@@ -0,0 +1,8 @@
+import styles from './ExploreGarden.module.css';
+export default function ExploreGarden(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/Pages/Home/ExplorePlants/ExplorePlants.js
@@ -0,0 +1,8 @@
+import styles from './ExplorePlant.module.css';
+export default function ExplorePlant(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
